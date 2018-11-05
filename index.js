da9a605ca475453c40380bc694c7ba2a17642d64
IvanOleinik <olejnik.iv@yandex.ua>
2018-11-05 23:05:05 +0200

    added removal over time

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use(function(req, res, next) {
@@ -14,25 +15,44 @@ app.use(function(req, res, next) {
 arrData = {};
 
 io.on('connection', function(socket){
-  socket.on('sending data', function(msg){
-  	arrData[msg['key']] = msg['data'];
-  });
+    socket.on('sending data', function(msg){
+        var currentDate = new Date();
+        currentDate.setTime(currentDate.getTime() + 30*60*1000);// время жизни данных 30 минут;
+        arrData[msg['key']] = [msg['data'], currentDate];
+    });
 });
 
 io.on('connection', function(socket){
-  socket.on('request data', function(msg){
-  	arrReturn = {"key" : msg, "data" : arrData[msg]}
-    io.emit('return data', arrReturn);
-  });
+    socket.on('request data', function(msg){
+        var currentDate = new Date();
+        if(arrData[msg] !== undefined){
+            if(arrData[msg][1] > currentDate){
+                //находим разницу во времени из за часового пояса
+                var timeDiff  = Math.abs(arrData[msg][1].getTimezoneOffset() * (60000));
+                //чтоб не изменять время в arrData[msg][1], записали время в timeZone
+                var timeZone = new Date(arrData[msg][1].getTime() + timeDiff);
+                arrReturn = {"key" : msg, "data" : arrData[msg], "time" : timeZone}
+                io.emit('return data', arrReturn);
+            }else{
+                delete arrData[msg]; //shift pop splice не подходят т.к. это ассоциативный массив
+                // который является обьектом.
+                arrReturn = null;
+                io.emit('return data', arrReturn);
+            }
+        }else{
+            arrReturn = null;
+            io.emit('return data', arrReturn);
+        }
+    });
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
